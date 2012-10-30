a520bd8916afc690d65711bed4a186ad4c2fd9fe
jozzez1 <jozze.zepl@hotmail.com>
2012-10-30 13:14:22 +0100

    Flags for execute.sh

--- Naloga11/execute.sh
+++ Naloga11/execute.sh
@@ -1,41 +1,61 @@
 #!/bin/tcsh
 
 # we pass the program arguments to the script
-set dim=($1)
-set con=($2)
-set seed=0
 
-if ( $# == 3 ) then
-	set seed=($3)
-endif
+while ($#argv > 0)
+	switch ($argv[1])
+		case -c:
+			shift
+			set compile=1
+			breaksw
+		case -N:
+			shift
+			set dim=$argv[1]
+			breaksw
+		case -D:
+			shift
+			set con=$argv[1]
+			breaksw
+		case -S:
+			shift
+			set seed=$argv[1]
+			breaksw
+		default:
+			set seed=0
+			set compile=0
+		endif
+	endsw
+	shift
+end
 
 # run the program ...
 ./prg $dim $con $seed
 
-set mfile="matrix-$1-$2.txt"
-set ofile="connectivity-$1-$2.txt"
-set gfile="graph-$1-$2.tex"
-set pfile="plot-$1-$2.tex"
-set efile="graph-$1-$2.eps"
-set mgraph="mgraph-$1-$2.tex"
-set mefile="mgraph-$1-$2.eps"
+set mfile="matrix-$dim-$con.txt"
+set ofile="connectivity-$dim-$con.txt"
+set graph="graph-$dim-$con"
+set mgraph="mgraph-$dim-$con"
+set pfile="plot-$dim-$con.tex"
 
 # replace the stuff in the template -- from the template we create the script
 sed -e 's|CONNECTIVITY|'"$ofile"'|' \
-	-e 's|NNODES|'"$1"'|' \
-	-e 's|NCONNECTIOS|'"$2"'|' \
-	-e 's|MGRAPH|'"$mgraph"'|' \
-	-e 's|GRAPH|'"$gfile"'|' \
+	-e 's|NNODES|'"$dim"'|' \
+	-e 's|NCONNECTIOS|'"$con"'|' \
+	-e 's|MGRAPH|'"$mgraph"'.tex|' \
+	-e 's|GRAPH|'"$graph"'.tex|' \
 	-e 's|MATRIX|'"$mfile"'|' < plot_template.gp > $pfile
 
 gnuplot $pfile
-epstopdf $efile
-epstopdf $mefile
+epstopdf $graph.eps
+epstopdf $mgraph.eps
 
 echo "$pfile created!"
-echo "Plots are $efile and $mefile."
+echo "Plots are $graph.tex and $mgraph.tex."
 
 # flags come into play
+if ( $compile = 1 ) then
+	pdflatex jozze_zobec_111.tex
+endif
 
 exit 0
 
