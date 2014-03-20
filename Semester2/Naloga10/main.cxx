73b00c845cce82e5d53cb8ef78dba6c0f07a6ca3
Jozze <jozze.zepl@hotmail.com>
2014-03-20 13:28:33 +0100

    plotting fully operational

--- Semester2/Naloga10/main.cxx
+++ Semester2/Naloga10/main.cxx
@@ -6,20 +6,47 @@
 
 int main (int argc, char ** argv)
 {
-    if (argc != 3)
+    if (argc != 2)
     {
-        std::cout << "Wrong program usage! Try:" 	<< std::endl;
-        std::cout << argv[0] << " <filename> <N>" 	<< std::endl;
+        std::cerr << "Wrong program usage! Try:" 	<< std::endl;
+        std::cerr << argv[0] << " <filename>" 	    << std::endl;
         exit (EXIT_FAILURE);
     }
 
+    // we get the filename
     std::string filename (argv[1]);
-    int N = atoi (argv[2]);
 
-    double * x = new double[N*N],
-           * y = new double[N*N],
-           * z = new double[N*N];
-    
+    // from the format we read N
+    // fortmat is -N-md.txt ...
+    size_t start    = filename.find_first_of ("-"),
+           stop     = filename.find_last_of  ("-"),
+           dot      = filename.find_last_of (".");
+
+    // sp this the int
+    size_t N = atoi ((filename.substr(start+1, stop-1)).c_str()) + 2;
+    std::string title = "Poves, \\Delta m = " + filename.substr(stop+1,dot);
+
+    mglData x (N, N),
+            y (N, N),
+            z (N, N),
+            X (13),
+            Y (13);
+
+    // let's also plot the outline of the weighted object
+    X.SetVal (0.25, 0); Y.SetVal (0, 0);
+    X.SetVal (0.25, 1); Y.SetVal (0.5, 1);
+    X.SetVal (0, 2);    Y.SetVal (0.5, 2);
+    X.SetVal (0, 3);    Y.SetVal (0.75, 3);
+    X.SetVal (0.25, 4); Y.SetVal (0.75, 4);
+    X.SetVal (0.25, 5); Y.SetVal (1, 5);
+    X.SetVal (0.5, 6);  Y.SetVal (1, 6);
+    X.SetVal (0.5, 7);  Y.SetVal (0.75, 7);
+    X.SetVal (1, 8);    Y.SetVal (0.75, 8);
+    X.SetVal (1, 9);    Y.SetVal (0.5, 9);
+    X.SetVal (0.5, 10); Y.SetVal (0.5, 10);
+    X.SetVal (0.5, 11); Y.SetVal (0, 11);
+    X.SetVal (0.25, 12);Y.SetVal (0, 12);
+
 
     std::ifstream fin (filename.c_str(), std::ifstream::in);
     if (!fin)
@@ -28,10 +55,27 @@ int main (int argc, char ** argv)
         exit (EXIT_FAILURE);
     }
 
-    for (int i = 0; !fin.eof(); i++)
-        fin >> x[i] >> y[i] >> z[i];
+    for (long i = 0; !fin.eof(); i++)
+        fin >> x.a[i] >> y.a[i] >> z.a[i];
     fin.close ();
 
+    // now we change the ending for output
+    filename.replace (filename.end()-3, filename.end(), "png");
+
+    mglGraph gr (0, 800, 800);
+    gr.SetRanges (x,y);
+    gr.SetRange ('c', z);
+    gr.Colorbar ("wyqrRk_");
+    gr.Axis("q");
+    gr.Box ("q");
+
+    gr.Cont (x, y, z, "t");
+    gr.Dens (x, y, z, "wyqrRk");
+    gr.Plot (X, Y, "W2");
+    gr.Title (title.c_str());
+
+    gr.WritePNG (filename.c_str(), "Poves");
+    gr.ShowImage ("feh");
     exit (EXIT_SUCCESS);
 }
 
