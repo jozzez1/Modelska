15fb072fd3dfa5b99acc037f4f41c927b0f1d58d
Jozze <jozze.zepl@hotmail.com>
2014-03-22 03:12:31 +0100

    fixed it with void functions

--- Semester2/Naloga11/main.c
+++ Semester2/Naloga11/main.c
@@ -6,34 +6,29 @@
 static inline double
 pow2 (double x) { return x*x; };
 
-gsl_vector *
-start (unsigned int N, double f0)
+void
+start (gsl_vector * v, unsigned int N, double f0)
 {
-    gsl_vector * v = gsl_vector_alloc (N);
     gsl_vector_set_all (v, f0);
-    return v;
 }
 
-gsl_vector *
-main_diagonal (const gsl_vector * f1)
+void
+main_diagonal (gsl_vector * main_diag, const gsl_vector * f1)
 {
     unsigned int N = f1->size,
                  i;
 
     double difference = gsl_vector_get (f1, 0) - gsl_vector_get (f1, 2);
-    gsl_vector * main_diag = gsl_vector_alloc (N-2);
     gsl_vector_set (main_diag, 0, -1 -0.25*pow2(difference));
     for (i = N-3; i--; )
     {
         difference = gsl_vector_get (f1, i+1) - gsl_vector_get (f1, i+3);
         gsl_vector_set (main_diag, i+1, -2 -0.25*pow2(difference));
     }
-
-    return main_diag;
 }
 
-gsl_vector *
-side_val (const gsl_vector * f1, const gsl_vector * f0, double ht)
+void
+side_val (gsl_vector * values, const gsl_vector * f1, const gsl_vector * f0, double ht)
 {
     unsigned int N = f1->size,
                  i;
@@ -41,8 +36,7 @@ side_val (const gsl_vector * f1, const gsl_vector * f0, double ht)
     double f11          = gsl_vector_get (f1,1),
            difference   = f11 - gsl_vector_get (f0, 1),
            h            = 1.0 / N,
-           hht2          = pow2 (h / ht);
-    gsl_vector * values = gsl_vector_alloc (N-2);
+           hht2         = pow2 (h / ht);
     gsl_vector_set (values, 0, (-1)*hht2 * pow2(difference) - h*sin (f11));
 
     for (i = N-3; i--;)
@@ -50,38 +44,29 @@ side_val (const gsl_vector * f1, const gsl_vector * f0, double ht)
         difference = gsl_vector_get (f1, i+2) - gsl_vector_get (f0, i+2);
         gsl_vector_set (values, i+1, (-1) * hht2 * pow2(difference));
     }
-
-    return values;
 }
 
-gsl_vector *
-Fnew (const gsl_vector * f1, const gsl_vector * f0, const gsl_vector * side_diag, double ht)
+void
+Fnew (gsl_vector * solution, gsl_vector * main_diag, gsl_vector * values,
+        const gsl_vector * f1, const gsl_vector * f0, const gsl_vector * side_diag, double ht)
 {
     unsigned int N = f0->size;
 
-    gsl_vector * main_diag  = main_diagonal (f1),
-               * values     = side_val (f1, f0, ht),
-               * solution   = gsl_vector_alloc (N);
-    
+    main_diagonal (main_diag, f1),
+    side_val (values, f1, f0, ht);    
     gsl_vector_view subsolution = gsl_vector_subvector (solution, 1, N-2);
 
     // now we solve this system
     gsl_linalg_solve_symm_tridiag (main_diag, side_diag, values, &subsolution.vector);
     gsl_vector_set (solution, N-1, 0);
     gsl_vector_set (solution, 0, sin(gsl_vector_get (f1, 0))/N - gsl_vector_get (solution, 1));
-
-    gsl_vector_free (main_diag);
-    gsl_vector_free (values);
-
-    return solution;
 }
 
-gsl_vector *
-fnew (const gsl_vector * F, const gsl_vector * f1, const gsl_vector * f0, double ht)
+void
+fnew (gsl_vector * f_updated, const gsl_vector * F, const gsl_vector * f1, const gsl_vector * f0, double ht)
 {
     unsigned int N = f0->size, i;
     double hth2 = pow2 (ht*N);
-    gsl_vector * f_updated = gsl_vector_alloc (N);
 
     for (i = N-2; i--; )
     {
@@ -105,49 +90,59 @@ fnew (const gsl_vector * F, const gsl_vector * f1, const gsl_vector * f0, double
 
     gsl_vector_set (f_updated, 0, (-1)*minus_fT0);
     gsl_vector_set (f_updated, 0, fTN);
-
-    return f_updated;
 }
 
 void
-time_step (gsl_vector * FT, gsl_vector * fT,
+time_step (gsl_vector * FT, gsl_vector * fT, gsl_vector * main_diag, gsl_vector * values,
         const gsl_vector * F, const gsl_vector * side_diag,
         const gsl_vector * f1, const gsl_vector * f0, double ht)
 {
-    fT = fnew (F, f1, f0, ht);
-    FT = Fnew (fT, f1, side_diag, ht);
+    fnew (fT, F, f1, f0, ht);
+    Fnew (FT, main_diag, values, fT, f1, side_diag, ht);
 }
 
 int main (int argc, char ** argv)
 {
-    unsigned int N  = 50,
-                 T  = 20000,
-                 i  = 0;
+    unsigned int N  = 100,
+                 T  = 2000,
+                 i;
     double ht       = 1e-3,
            fi0      = M_PI*(0.5 + 0.3);
 
-    gsl_vector * f0         = start (N, fi0),
-               * f1         = start (N, fi0),
-               * side_diag  = start (N-3, 1),
-               * FT         = Fnew (f1, f0, side_diag, ht),
+    // allocation phase
+    gsl_vector * f0         = gsl_vector_alloc (N),
+               * f1         = gsl_vector_alloc (N),
+               * FT         = gsl_vector_alloc (N),
                * fT         = gsl_vector_alloc (N),
-               * F          = gsl_vector_alloc (N);
-    
+               * F          = gsl_vector_alloc (N),
+               * main_diag  = gsl_vector_alloc (N-2),
+               * side_diag  = gsl_vector_alloc (N-3),
+               * values     = gsl_vector_alloc (N-2);
+
+    // now we fill them
+    gsl_vector_set_all (f0, fi0);
+    gsl_vector_set_all (f1, fi0);
+    gsl_vector_set_all (side_diag, 1);
+    Fnew (FT, main_diag, values, f1, f0, side_diag, ht);
+
     for (i = T; i--; )
     {
-        F = FT;
-        time_step (FT, fT, F, side_diag, f1, f0, ht);
-
-        f0 = f1;
-        f1 = fT;
+        gsl_vector_memcpy (F, FT);
+        time_step (FT, fT, main_diag, values, F, side_diag, f1, f0, ht);
+        gsl_vector_memcpy (f0, f1);
+        gsl_vector_memcpy (f1, fT);
     }
+
+    gsl_vector_fprintf (stdout, FT, "%f");
 
     gsl_vector_free (f0);
     gsl_vector_free (f1);
-    gsl_vector_free (side_diag);
     gsl_vector_free (FT);
     gsl_vector_free (fT);
     gsl_vector_free (F);
+    gsl_vector_free (side_diag);
+    gsl_vector_free (main_diag);
+    gsl_vector_free (values);
 
     return 0;
 }
