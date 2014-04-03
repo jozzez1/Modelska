cf83a044a51bf5d0543a0c8e3f3c79b12e63a5c4
Jozze <jozze.zepl@hotmail.com>
2014-04-03 20:22:25 +0200

    I decided that epsilon will be initial parameter

--- Finale/star_system.c
+++ Finale/star_system.c
@@ -1,7 +1,7 @@
 #include "star_system.h"
 
 void
-init_system (binary * u, double M1, double M2, double p_phi, double p_rho)
+init_system (binary * u, double M1, double M2, double p_phi, double epsilon)
 {
     double mu       = M1 * M2 /(M1 + M2),
            alpha    = 8 * mu * mu * M1 * M2;
@@ -13,22 +13,13 @@ init_system (binary * u, double M1, double M2, double p_phi, double p_rho)
     assert (p_phi < Limit_p_phi);
     assert (p_phi >= limit_p_phi);
 
-    double p_phi2       = p_phi * p_phi,
-           Limit_p_rho  = sqrt(4*M1*M2 - p_phi2),
-           limit_p_rho  = sqrt(4*M1*M2 - p_phi2 - alpha/p_phi2);
-
-    assert (p_rho < Limit_p_rho);
-    assert (p_rho >= limit_p_rho);
-
     // so far so good, all the parameters were accepted
     u->M1       = M1;
     u->M2       = M2;
     u->mu       = mu;
     u->alpha    = alpha;
     u->p_phi    = p_phi;
-    u->p_rho    = p_rho;
-    u->E        = 0.5*(p_phi2 + p_rho*p_rho) - 2*M1*M2;
-    u->epsilon  = sqrt(1 + 2*u->E*p_phi2/(alpha*alpha));
+    u->epsilon  = epsilon;
 }
 
 void
@@ -40,4 +31,10 @@ trajectory (double * rho, double * phi,
     *rho = (sys.p_phi * sys.p_phi) / (sys.alpha * (1 + sys.epsilon*cos(*phi)));
 }
 
+double
+solar_year (binary sys)
+{
+    double T = 2*M_PI / pow(1 - sys.epsilon * sys.epsilon, 1.5);
+    return T * sys.p_phi * sys.p_phi * sys.p_phi/(sys.alpha * sys.alpha);
+}
 
--- Finale/star_system.h
+++ Finale/star_system.h
@@ -10,12 +10,10 @@ typedef struct binary
 {
     double epsilon, // eccentricity of the orbits
            alpha,   // potential well strength
-           E,       // energy
            mu,      // reduced mass of the system
            M1,      // mass of the 1st star
            M2,      // mass of the 2nd star
-           p_phi,   // angular momentum
-           p_rho;   // radial momentum
+           p_phi;   // angular momentum
 } binary;
 
 // initialize the system
@@ -23,5 +21,8 @@ void init_system (binary * sys, double M1, double M2, double p_phi, double p_rho
 
 // calculate stuff from parameters and time, like the trajectory :D
 void trajectory (double * rho, double * phi, binary sys, double t);
+
+// return the period of such a binary system
+double solar_year (binary sys);
 
 #endif
